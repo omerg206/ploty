e335ed92105d67087245b71594ac4969c61b4395
og17612 <ogolan1@elta.co.il>
2021-04-27 18:19:18 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import * as Plotly from './plotly-latest.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild("tester") ele: ElementRef;
+
   title = 'ploty';
+
+  createData() {
+    const xAxsisSize = 100;
+    const yAxsisSize = 100;
+    let i = 0;
+    let j = 0;
+
+    let res = [];
+
+    for (i = 0; i < xAxsisSize; ++i) {
+      let temp = [];
+
+      for (j = 0; j < yAxsisSize; ++j) {
+        temp.push(Math.pow(i - j, 2))
+
+      }
+      res.push(temp)
+    }
+
+
+    return res;
+  }
+
+
+  ngOnInit(): void {
+    const ele = this.ele.nativeElement;
+    const z = this.createData();
+    var data = [
+      {
+        z,
+        type: 'heatmap',
+        colorscale: [[0, "white"], [1, "black"]]
+      }
+    ];
+
+    Plotly.newPlot(ele, data);
+  }
+
+  ngAfterViewInit(): void {
+  }
 }
