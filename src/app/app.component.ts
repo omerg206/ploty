47468b311aeefae9c4015085630ae27040708669
og17612 <ogolan1@elta.co.il>
2021-04-28 15:13:33 +0300

    plotty zoom and oher buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
-import * as Plotly from './plotly-latest.min.js';
+import { ModeBarDefaultButtons, Config,newPlot } from "plotly.js";
+import * as Plotly from 'plotly.js-dist'
+// import * as Plotly from 'plotly.js/dist/plotly.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
 
 
   @ViewChild("tester") ele: ElementRef;
 
   title = 'ploty';
 
-  createData() {
-    const xAxsisSize = 100;
-    const yAxsisSize = 100;
+  createData(xAxsisSize:number, yAxsisSize: number) {
     let i = 0;
     let j = 0;
 
@@ -37,19 +37,79 @@ export class AppComponent implements OnInit, AfterViewInit {
 
 
   ngOnInit(): void {
-    const ele = this.ele.nativeElement;
-    const z = this.createData();
-    var data = [
-      {
-        z,
-        type: 'heatmap',
-        colorscale: [[0, "white"], [1, "black"]]
-      }
-    ];
-
-    Plotly.newPlot(ele, data);
+  
+   
+  this.drawHeatMap(1000,1000, "Heatmap");
   }
 
   ngAfterViewInit(): void {
   }
+
+drawHeatMap(xAxsisSize:number, yAxsisSize: number, title: string){
+  const plotyHTMLEle = this.ele.nativeElement;
+    const z = this.createData(xAxsisSize,yAxsisSize);
+    var data = [
+      {
+        z,
+        type: 'heatmap',
+        // colorscale: [[0, "blue"],  [0.75, "orange"], [1, "red"]]
+      }
+    ];
+
+    const layout = {
+      title
+    }
+
+
+    const modeBarButtons: Array<Array<ModeBarDefaultButtons>> = [
+      ["toImage"],
+      ["autoScale2d"],
+      ["pan2d"],
+      ["resetViews"],
+      ["zoom2d"], ["zoomIn2d"],
+      ["zoomOut2d"],
+      ["resetScale2d"],
+      ["select2d"],
+      ["tableRotation"],
+      ["lasso2d"],
+      ["orbitRotation"], 
+      ["toggleHover"],
+      ["pan3d"],
+      ["toggleSpikelines"],
+      ["hoverCompareCartesian"]
+    ]
+
+    const config:  Partial<Config> =  {
+      displayModeBar: true, scrollZoom: true, displaylogo: false, responsive: true,
+       modeBarButtons, autosizable: true, 
+    }
+
+    plotyHTMLEle.removeAllListeners((bla) => {
+      alert(bla)
+    });
+
+    const heatmap = Plotly.newPlot(plotyHTMLEle, data as any, layout,config);
+    this.addEventListnersHeatMap(plotyHTMLEle);
+    
+}
+
+
+
+addEventListnersHeatMap(plotyHTMLEle){
+  plotyHTMLEle.on('plotly_relayout', (event) => {
+    console.log(  `$plotly_relayout ${JSON.stringify(event)}`);
+   
+    if (event["xaxis.autorange"]) {
+      this.drawHeatMap(100, 100, "Heatmap zoom")
+    } else if (this.isZoomEvent(event)) {
+      this.drawHeatMap(3, 3, "Heatmap zoom")
+    }
+   })
+}
+
+isZoomEvent(event: Object): boolean{
+  return Object.keys(event).some((key:string)=> key.includes("range["))
+}
+
+
 }
